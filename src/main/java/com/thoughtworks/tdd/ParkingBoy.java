84e32a8a7d1364ceaab5b08d3f32c1c9953ab41f
1134923982 <1134923982@qq.com>
2019-07-13 12:50:48 +0800

    feat:implement Story1 AC1

---
+++ src/main/java/com/thoughtworks/tdd/ParkingBoy.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.tdd;
+
+
+public class ParkingBoy {
+    private ParkingLot parkingLot;
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public Ticket park(Car car) {
+        Ticket ticket = parkingLot.park(car);
+        return ticket;
+    }
+
+    public Car fetch(Ticket ticket) {
+        return parkingLot.getCar(ticket);
+    }
+}
